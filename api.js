76769e0ecc245106639b19f98530c63aa74551f8
Krissanawat <krissanawat101@gmail.com>
2020-01-16 11:10:06 +0700

    try auto deploy

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 require("./db")
 
-router.use(require("./api_auth"))
+router.use(require("./api_auth"));
 // router.use(require("./api_product"))
 // router.use(require("./api_employee"))
 // router.use(require("./api_customer"))
