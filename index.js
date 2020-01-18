a678e222f4b869a6b5df0ff79b60df5d4fb8d8af
Krissanawat <krissanawat101@gmail.com>
2020-01-18 22:07:00 +0700

    add Cors rule 3

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const cors = require("cors");
 app.use(express.static(__dirname + "/uploaded"));
 require("./db");
 const Users = require("./models/user_schema");
-var whitelist = ["http://localhost", "https://basicpos.netlify.com/"];
-var corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://basicpos.netlify.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return res.json({status:'error',msg});
     }
+    return callback(null, true);
   }
-};
-app.use(cors(corsOptions));
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
