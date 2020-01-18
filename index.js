c2a065a5d4fd67d5930c9d30eeae13909c547ddc
Krissanawat <krissanawat101@gmail.com>
2020-01-18 21:39:17 +0700

    add Cors rule 2

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const cors = require("cors");
 app.use(express.static(__dirname + "/uploaded"));
 require("./db");
 const Users = require("./models/user_schema");
+var whitelist = ["http://localhost", "https://basicpos.netlify.com/"];
 var corsOptions = {
-  origin: "https://basicpos.netlify.com/",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
