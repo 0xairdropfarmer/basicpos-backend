418ebbe059508ae9ec6b2f69c6ca4fce22a9aba7
krissanawat <krissnawat@hey.com>
2020-09-01 19:05:45 +0700

    Chapter 14 :  Export PDF, Excel,CSV,Bulk Delete,Inline Editing

--- api_pos_machine.js
+++ api_pos_machine.js
@@ -27,6 +27,7 @@ router.get("/pos_machine/:id", async (req, res) => {
     res.json({ result: "error", message: err.msg });
   }
 });
+
 router.post("/pos_machine", async (req, res) => {
   try {
 
@@ -56,6 +57,33 @@ router.put("/pos_machine", async (req, res) => {
     res.json({ result: "error", message: err.msg });
   }
 });
+router.put("/pos_machine/inline_update", async (req, res) => {
+  let id = req.body._id
+  let column = req.body.column
+  let value = req.body.value
+  // console.log(column)
+  try {
+    let doc = await POS_Machine.update(
+      { _id: id }, { $set: { [column]: value } }, function (err, result) {
+
+        if (err) {
+          res.json({ result: "error", message: err.msg });
+        }
+        else {
+          res.json({
+            result: result,
+            message: "Update POS data Successfully",
+          });
+        }
+
+      }
+    );
+
+
+  } catch (err) {
+    res.json({ result: "error", message: err.msg });
+  }
+});
 router.delete("/pos_machine/:id", async (req, res) => {
   // console.log(req.params.id);
   try {
@@ -68,5 +96,25 @@ router.delete("/pos_machine/:id", async (req, res) => {
   } catch (err) {
     res.json({ result: "error", message: err.msg });
   }
+});
+router.delete("/pos_machine/bulk_delete/:id", async (req, res) => {
+  let id = req.params.id.split(",");
+  console.log(id)
+  // try {
+  let response = await POS_Machine.deleteMany({ _id: { $in: id } }, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json({
+        result: "success",
+        message: "Bulk Delete Successfully",
+        data: data,
+      });
+    }
+  });
+
+  // } catch (err) {
+  //   res.json({ result: "error", message: err.msg });
+  // }
 });
 module.exports = router;
--- db.js
+++ db.js
@@ -1,21 +1,24 @@
 var mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URL);
-
-mongoose.connection.on("connected", function() {
+mongoose.set('debug', true);
+mongoose.set("debug", (collectionName, method, query, doc) => {
+  console.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
+});
+mongoose.connection.on("connected", function () {
   console.log("Mongoose default connection open");
 });
 
-mongoose.connection.on("error", function(err) {
+mongoose.connection.on("error", function (err) {
   console.log("Mongoose default connection error: " + err);
 });
 
-mongoose.connection.on("disconnected", function() {
+mongoose.connection.on("disconnected", function () {
   console.log("Mongoose default connection disconnected");
 });
 
-process.on("SIGINT", function() {
-  mongoose.connection.close(function() {
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
     console.log(
       "Mongoose default connection disconnected through app termination"
     );
